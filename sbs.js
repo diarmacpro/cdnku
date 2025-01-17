e941be734653c965c398ceee8d76e1706a58dc72
diarmacpro <164306202+diarmacpro@users.noreply.github.com>
2025-01-18 01:01:37 +0700

    Create sbs.js

---
+++ sbs.js
@@ -0,0 +1,55 @@
+class sbs {
+  constructor(url, apiKey, tableName) {
+    this.client = createClient(url, apiKey);
+    this.tableName = tableName;
+  }
+
+  async g(table, filters = null, callback) {
+    let query = this.client.from(table).select("*");
+    if (filters) {
+      Object.entries(filters).forEach(([key, value]) => {
+        query = query.eq(key, value);
+      });
+    }
+    try {
+      const { data, error } = await query;
+      callback(data, error);
+    } catch (err) {
+      callback(null, err);
+    }
+  }
+
+  async i(data, callback) {
+    try {
+      const query = this.client.from(this.tableName).insert(data).select('*');
+      const { data: result, error } = await query;
+      callback(error ? null : result, error);
+    } catch (err) {
+      callback(null, err);
+    }
+  }
+
+  async d(filters, callback) {
+    try {
+      const query = this.client.from(this.tableName).delete().match(filters);
+      const { data, error } = await query;
+      callback(error ? null : data, error);
+    } catch (err) {
+      callback(null, err);
+    }
+  }
+
+  async u(data, filters, callback) {
+    try {
+      let query = this.client.from(this.tableName).update(data);
+      Object.entries(filters).forEach(([key, value]) => {
+        query = query.eq(key, value);
+      });
+      query = query.select('*');
+      const { data: result, error } = await query;
+      callback(error ? null : result, error);
+    } catch (err) {
+      callback(null, err);
+    }
+  }
+}
